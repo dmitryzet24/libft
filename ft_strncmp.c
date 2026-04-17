3bd4aa2b37ad2dce39005d0a0cec33eefc9b5ce1
Dmitrii Andrushchenko <dandrush@c1r11s9.42porto.com>
2026-04-17 14:35:50 +0100

    ft_strncmp.c

---
+++ ft_strncmp.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dandrush <dandrush@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/17 14:34:19 by dandrush          #+#    #+#             */
+/*   Updated: 2026/04/17 14:35:20 by dandrush         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (s1[i] == s2[i] && s1[i] != '\0' && i < n - 1)
+		i++;
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
