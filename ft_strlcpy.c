e4ec59832f141660751fe19d89d4517fab4f9638
Dmitrii <dandrush@student.42porto.com>
2026-04-17 14:58:33 +0100

    ft_strlcpy.c

---
+++ ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dandrush <dandrush@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/17 14:57:38 by dandrush          #+#    #+#             */
+/*   Updated: 2026/04/17 14:57:40 by dandrush         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	src_len;
+
+	src_len = 0;
+	i = 0;
+	while (src[src_len])
+		src_len++;
+	if (size == 0)
+		return (src_len);
+	while (src[i] && i < (size - 1))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (src_len);
+}
--- libft.h
+++ libft.h
@@ -5,7 +5,7 @@
 
 int		ft_isalpha(unsigned char *str);
 int		ft_isdigit(unsigned char *str);
-
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 int		ft_strlen(char *str);
 int		ft_strncmp(char *s1, char *s2, unsigned int n);
 char	*ft_tolower(char *str);
