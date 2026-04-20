7eec7c4b6f7c242309ee199fc8086a6f41ee5fdb
Dmitrii <dandrush@student.42porto.com>
2026-04-20 15:59:13 +0100

    drafting tests

--- tests.c
+++ tests.c
@@ -1,17 +1,30 @@
-void	test_int_f_int()
+#include "libft_st.h"
+#include "libft.h"
+
+int	test_int_f_int()
 {
 	pid_t	pid = fork();
 
 	if (pid == 0) {
-		// Custom code here
+		printf("Child process\n");
 		exit (0);
 	}
 	else {
 		int	status;
-
+		printf("Parent process\n");
 		waitpid(pid, &status, 0);
-		id (WIFSIGNALED(status)) {
+		if (WIFSIGNALED(status)) {
 			printf("The test crashed\n");
+			return (1);
 		}
 	}
+	return(0);
 }
+
+int main(){
+	int	numbers[] = {1, 0, 257, (-256)};
+
+	test_int_f_int();
+	return (0);
+}
+
