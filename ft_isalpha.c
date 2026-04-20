7d4cd4872190d086354f6970ff9170864b84ae83
Dmitrii <dandrush@student.42porto.com>
2026-04-20 13:26:34 +0100

    draft isalpha

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,25 +6,16 @@
 /*   By: dandrush <dandrush@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/17 13:39:39 by dandrush          #+#    #+#             */
-/*   Updated: 2026/04/17 14:47:44 by dandrush         ###   ########.fr       */
+/*   Updated: 2026/04/20 13:22:36 by dandrush         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalpha(unsigned char *str)
+int	ft_isalpha(int c) //returns the bitmask
 {
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (!((str[i] >= 'A' && str[i] <= 'Z')
-				|| (str[i] >= 'a' && str[i] <= 'z')))
-		{
-			return (0);
-		}
-		i++;
-	}
+	if (!((c >= 'A' && c <= 'Z')
+			|| (c >= 'a' && c <= 'z')))
+		return (0);
 	return (1);
 }
